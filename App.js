e24ae5f391739a2e037969d539f721434896238f
reutKeyshawn <reki8611@gmail.com>
2022-08-22 20:42:51 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
+const top = 50;
+const left = 30;
+const right = 30;
+
+const HelloWorldApp = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello, {props.name}!</Text>
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("./photos/beer-bottle2.png")}
+      />
+      <Circle></Circle>
+    </View>
+  );
+};
+
+const Circle = () => {
+  return <View style={styles.circle} />;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 300,
+    width: 300,
+    transform: [{ rotate: "90deg" }],
+  },
+  circle: {
+    top: top,
+    right: right,
+    left: left,
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    backgroundColor: "yellow",
   },
 });
+
+export default App;
