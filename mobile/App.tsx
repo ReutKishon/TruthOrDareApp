4f4bd27f2cbe38153e990a7eadab8da281c1023f
Elad Kishon <eladk@jfrog.com>
2022-09-15 14:38:53 -0400

    link routes for faster debugging

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,17 +3,24 @@ import { StyleSheet, SafeAreaView } from "react-native";
 import {Provider} from "react-redux";
 import store from "./app/store";
 import {NavigationContainer} from "@react-navigation/native";
-import {GameNavigator} from "./routes/navigator";
 import Home from "./screens/Home";
 import Start from "./screens/Start";
 import ExistingGame from "./screens/Join";
 import Main from "./screens/Main";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 const App = () => {
-  return (
+    const GameNavigator = createNativeStackNavigator();
+    return (
       <Provider store={store}>
           <SafeAreaView style={styles.container}>
-              <NavigationContainer>
+              <NavigationContainer linking={{
+                  prefixes: [],
+                  config: {
+                      screens: {
+                          Main: 'game',
+                      },
+                  }}}>
                   <GameNavigator.Navigator>
                         <GameNavigator.Screen name="Home" component={Home} />
                         <GameNavigator.Screen name="Start" component={Start} />
